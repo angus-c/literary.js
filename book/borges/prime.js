b8bdef26c572708da8620933944bda1df44dfc96
Chris Johnson <me@chrismj.net>
2015-01-06 08:51:30 +0000

    Adding a variable declaration to stepsUntrodden
    This will fix a global variable leak.

--- book/borges/prime.js
+++ book/borges/prime.js
@@ -2,7 +2,7 @@
 // of hidden spandrels and eternally clambering, broad-gaited beasts...
 
 var monstersAscendingAStaircase = function(numberOfSteps) {
-  var stairs = []; stepsUntrodden = [];
+  var stairs = []; var stepsUntrodden = [];
   var largestGait = Math.sqrt(numberOfSteps);
 
   // A succession of creatures mount the stairs;
